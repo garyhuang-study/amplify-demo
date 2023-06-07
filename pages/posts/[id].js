4bacf760130747dd3648572f7a444c3cfcec7536
Gary_Huang <gary_huang@perfectcorp.com>
2023-06-07 11:23:58 +0800

    [what] add timestamp on ssr props. 1

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,23 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
-const Post = () => {
+const Post = (props) => {
   const router = useRouter()
   const { id } = router.query
 
-  return <p>Post: {id}</p>
+  return (
+    <>
+      <p>Post: {id}</p>
+      <br />
+      <p>Latest Modified: {props.latestModified}</p>
+    </>
+  )
 }
 
 export default Post
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      latestModified: Date.now()
+    },
+  };
+};
